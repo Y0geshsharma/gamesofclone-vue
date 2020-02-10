2f054e8a454dee1cbfcfea9e4e31128b43079b71
y0geshsharma <yogeshniit2016@gmail.com>
2020-02-10 14:46:52 +0530

    made a separate constant file

---
+++ src/constant.js
@@ -0,0 +1,16 @@
+const GET_DROID = 'http://localhost:3001/droid'
+const GET_TROPPER = ''
+const GET_HISTORY = ''
+const GET_RESULT = ''
+const POST_DROID = ''
+const POST_TROPPER = ''
+const POST_RESULT = ''
+exports.QUERRY = {
+  GET_DROID,
+  GET_TROPPER,
+  GET_HISTORY,
+  GET_RESULT,
+  POST_DROID,
+  POST_TROPPER,
+  POST_RESULT
+}
